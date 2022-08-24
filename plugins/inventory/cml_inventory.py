d3a87fcb1cb3b52f63180e87ebb9bc375561545c
Steven Carter <stevenca@cisco.com>
2022-08-24 12:50:50 -0500

    Update cml_inventory.py
    Added display of cml_group back.

--- plugins/inventory/cml_inventory.py
+++ plugins/inventory/cml_inventory.py
@@ -129,6 +129,7 @@ class InventoryModule(BaseInventoryPlugin):
             self.group = 'cml_hosts'
 
         self.display.vvv("cml.py - Group: {0}".format(self.group))
+        self.inventory.set_variable('all', 'cml_group', self.group)
 
         self.inventory.set_variable('all', 'cml_host', self.host)
         self.inventory.set_variable('all', 'cml_username', self.username)
