585b9e80a3ed30159a6ad36da41ace5fabf1e09e
Matt Tarkington <mtarking@cisco.com>
2023-12-21 09:10:23 -0500

    add if in condition for starting lab

--- plugins/modules/cml_lab.py
+++ plugins/modules/cml_lab.py
@@ -162,7 +162,7 @@ def run_module():
                 lab.start(wait=cml.params['wait'])
             lab.title = cml.params['lab']
             cml.result['changed'] = True
-        elif lab.state() == "STOPPED":
+        elif lab.state() in ("DEFINED_ON_CORE", "STOPPED"):
             lab.start(wait=cml.params['wait'])
             cml.result['changed'] = True
     elif cml.params['state'] == 'absent':
